9c2d414cde24bce98e173520324825fdd56103c4
cjjjjjk <ho.vanhai03062004@gmail.com>
2025-06-21 11:17:56 +0700

    initial comit.

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<script setup lang="ts">
+import { RouterLink, RouterView } from 'vue-router'
+import HelloWorld from './components/HelloWorld.vue'
+</script>
+
+<template>
+  <header class="header">
+    <img alt="Vue logo" class="logo" src="@/assets/logo-192x192.png" width="125" height="125" />
+    <div class="wrapper">
+      <HelloWorld msg="Hi there 👋" />
+
+      <!-- <nav>
+        <RouterLink to="/">Home</RouterLink>
+        <RouterLink to="/about">About</RouterLink>
+      </nav> -->
+    </div>
+  </header>
+
+  <!-- <RouterView /> -->
+</template>
+
+<style scoped>
+.header {
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+  gap: 2rem;
+}
+</style>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+defineProps<{
+  msg: string
+}>()
+</script>
+
+<template>
+  <div class="greetings">
+    <h1 class="green">{{ msg }}</h1>
+  </div>
+</template>
+
+<style scoped>
+h1 {
+  font-weight: bolder;
+}
+
+h1:hover {
+  color: black;
+  transition:all;
+  transition-duration: 1s;
+}
+</style>
