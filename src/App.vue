3ea8cf3a993f0356861132cba4eac9fba72843ac
cjjjjjk <ho.vanhai03062004@gmail.com>
2025-06-22 21:55:16 +0700

    update: tailwind setup

--- src/App.vue
+++ src/App.vue
@@ -7,15 +7,13 @@ import HelloWorld from './components/HelloWorld.vue'
   <header class="header">
     <img alt="Vue logo" class="logo" src="@/assets/logo-192x192.png" width="125" height="125" />
     <div class="wrapper">
-      <HelloWorld msg="Hi there 👋." />
-
+      <HelloWorld msg="Hi there 👋" />
       <!-- <nav>
         <RouterLink to="/">Home</RouterLink>
         <RouterLink to="/about">About</RouterLink>
       </nav> -->
     </div>
   </header>
-
   <!-- <RouterView /> -->
 </template>
 
