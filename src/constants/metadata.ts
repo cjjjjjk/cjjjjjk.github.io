d32425f4b6a6aa0021164099cb73ad66a0baac44
cjjjjjk <ho.vanhai03062004@gmail.com>
2025-07-18 16:08:59 +0700

    fix: use cloudflare proxy for gitHub API requests

--- src/constants/metadata.ts
+++ src/constants/metadata.ts
@@ -1,4 +1,5 @@
 export const APP_METADATA = {
     USERNAME: 'cjjjjjk',
-    AUTHOR: 'Hai HV'
+    AUTHOR: 'Hai HV',
+    GITHUB_PROXY_URL: "https://cjjjjj-proxy.hai-hv04.workers.dev"
 }
--- src/services/enviroments.ts
+++ src/services/enviroments.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 import { APP_METADATA } from "@/constants/metadata";
 export const githubAPI = axios.create({
-    baseURL: 'https://api.github.com',
+    baseURL: APP_METADATA.GITHUB_PROXY_URL,
     timeout: 10000
 })
